147bf556cb77ad3d0b649a9575f647ba65cde0aa
Denis Eroshevskii <devskjj@gmail.com>
2025-11-21 02:51:48 +0600

    "task_2 Почистил код от лишнего, сделал рефактор, добавил метод, создающий путь к файлам в папке Data, из URI"

--- src/Server/Utility.java
+++ src/Server/Utility.java
@@ -1,68 +1,41 @@
 package Server;
 
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.*;
-import java.net.URI;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+
+import java.nio.file.Path;
 
 public class Utility {
     public static void initRoutes(HttpServer server) {
+
         server.createContext("/", exchange -> showRoute(exchange, "Это корневой путь."));
         server.createContext("/apps/", exchange -> showRoute(exchange, "Это путь приложения."));
         server.createContext("/apps/profile", exchange -> showRoute(exchange, "Это путь профиля."));
     }
 
-    private static void showRoute(HttpExchange exchange, String msg) throws IOException {
+    private static void sendResponse(HttpExchange exchange, int statusCode, String msg) throws IOException {
         byte[] bytes = msg.getBytes(StandardCharsets.UTF_8);
 
         exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=UTF-8");
-        exchange.sendResponseHeaders(200, bytes.length);
+        exchange.sendResponseHeaders(statusCode, bytes.length);
 
-        try (OutputStream outputStream = exchange.getResponseBody()) {
-            outputStream.write(bytes);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(bytes);
         }
     }
 
-    public static void writeHeaders(Writer writer, String type, Headers headers) {
-        write(writer, type, "");
-        headers.forEach((k, v) -> write(writer, "\t" + k, v.toString()));
+    private static void showRoute(HttpExchange exchange, String msg) throws IOException {
+        sendResponse(exchange, 200, msg);
     }
 
-    public static void write(Writer writer, String msg, String method) {
-        String data = String.format("%s:%s%n%n", msg, method);
-        try {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static Path getRequestUrlPath(HttpExchange exchange) {
+        String uri = exchange.getRequestURI().getPath();
+        if (uri.startsWith("/")) {
+            uri = uri.substring(1);
         }
-    }
-
-    public static PrintWriter getWriterFrom(HttpExchange exchange) {
-        OutputStream outputStream = exchange.getResponseBody();
-        Charset charset = StandardCharsets.UTF_8;
-        return new PrintWriter(outputStream, false, charset);
-    }
-
-    public static BufferedReader getReader(HttpExchange exchange) {
-        InputStream inputStream = exchange.getRequestBody();
-        Charset charset = StandardCharsets.UTF_8;
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, charset);
-        return new BufferedReader(inputStreamReader);
-    }
-
-    public static void writeData(Writer writer, HttpExchange exchange) {
-        try (BufferedReader reader = getReader(exchange)) {
-            if (!reader.ready()) return;
-
-            write(writer, "Data", "");
-            reader.lines().forEach(v -> write(writer, "\t", v));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return Path.of("src/Data", uri);
     }
 }
